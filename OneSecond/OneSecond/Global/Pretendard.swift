457f4f588f0f426d37ad44c76d57935232210d91
Seo <kirin2211@naver.com>
2024-06-17 11:04:57 +0900

    [ENH] Edit info.plist, Add Font, Folder

---
+++ OneSecond/OneSecond/Global/Pretendard.swift
@@ -0,0 +1,21 @@
+//
+//  Pretendard.swift
+//  OneSecond
+//
+//  Created by Doran on 6/17/24.
+//
+
+import Foundation
+
+enum Pretendard: String {
+    case black = "Pretendard-Black"
+    case bold = "Pretendard-Bold"
+    case extraBold = "Pretendard-ExtraBold"
+    case extraLight = "Pretendard-ExtraLight"
+    case light = "Pretendard-Light"
+    case medium = "Pretendard-Medium"
+    case regular = "Pretendard-Regular"
+    case semiBold = "Pretendard-SemiBold"
+    case thin = "Pretendard-Thin"
+}
+
