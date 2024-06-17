db7e1a9610a39912487928c238fdc59c25ebd37f
Seo <kirin2211@naver.com>
2024-06-17 10:29:18 +0900

    [ENH] Create Project

---
+++ OneSecond/OneSecond/App/OneSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneSecondApp.swift
+//  OneSecond
+//
+//  Created by Doran on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OneSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
