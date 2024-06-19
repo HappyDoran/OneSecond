38195a6ac242c15d0e128c85bbb63ea36cd117c6
Seo <kirin2211@naver.com>
2024-06-19 20:18:16 +0900

    [Feat#13]Create QuizFinishView 퀴즈가 끝나고 난 뒤의 뷰 완성 퀴즈 뷰 레이아웃 수정 navigation 안되던 상황 onboading의 NavigationLink -> NavigationStack으로 변경

--- OneSecond/OneSecond/Views/QuizFinishView.swift
+++ OneSecond/OneSecond/Views/QuizFinishView.swift
@@ -12,9 +12,12 @@ struct QuizFinishView: View {
         Text("퀴즈가 끝났습니다!")
         
         Button(action:{
-            
+            NavigationManager.shared.pop(to: .selectYear)
         }){
-            
+            ZStack{
+                RoundedRectangle(cornerRadius: 40).frame(width: 200,height: 50).foregroundColor(Color(hex: "#000000")).shadow(color: .gray, radius: 1, x: 0, y: 3)
+                Text("홈 화면으로 이동").font(.system(size: 18, weight: .bold)).foregroundColor(.white)
+            }
         }
     }
 }
--- OneSecond/OneSecond/Views/QuizView.swift
+++ OneSecond/OneSecond/Views/QuizView.swift
@@ -24,39 +24,43 @@ struct QuizView: View {
                 Rectangle()
                     .foregroundColor(Color(hex: "787880"))
                     .opacity(0.16)
-                    .frame(width: 345.0, height: 8.0)
+                    .frame(width:UIScreen.main.bounds.width - 32,height: 8.0)
                     .cornerRadius(4.0)
                 Rectangle()
                     .foregroundColor(Color(hex: "FF0080"))
-                    .frame(width: (Double(currentQuestionCount) / Double(questionCount)) * 345.0, height: 8.0)
+                    .frame(width: (Double(currentQuestionCount) / Double(questionCount)) * (UIScreen.main.bounds.width - 32), height: 8.0)
                     .cornerRadius(4.0)
                     .animation(.easeInOut, value: currentQuestionCount)
             }
+            HStack{
+                Spacer()
+                Text("\(currentQuestionCount)/\(questionCount)").font(.system(size: 12, weight: .semibold))
+            }
             Spacer()
             
-            Text("당신의 음악 감각을 보여주세요!").font(.system(size: 24, weight: .semibold))
+            Text("당신의 음악 감각을 보여주세요!").font(.system(size: 24, weight: .bold))
             
             ZStack{
                 if let artwork = musicManager.song?.artwork {
-                    ArtworkImage(artwork,width: 260, height: 260).cornerRadius(50)
+                    ArtworkImage(artwork,width: 260, height: 260).cornerRadius(50).shadow(color: .gray, radius: 1, x: 0, y: 3)
                 }
                 if(hintCount > 2){
-                    RoundedRectangle(cornerRadius: 50).frame(width: 260,height: 260).foregroundColor(Color(hex: "#D9D9D9"))
+                    RoundedRectangle(cornerRadius: 50).frame(width: 260,height: 260).foregroundColor(Color(hex: "#D9D9D9")).shadow(color: .gray, radius: 1, x: 0, y: 3)
                 }
             }
             .padding(.top,20)
-            
+
             if let title = musicManager.song?.title {
-                Text(title).font(.system(size: 20, weight: .semibold)).padding(.top,30).opacity(isCorrect ? 1 : 0)
+                Text(title).font(.system(size: 20, weight: .bold)).padding(.top,10).opacity(isCorrect ? 1 : 0)
             }
             
             if let artistName = musicManager.song?.artistName {
-                Text(artistName).padding(.top,1).opacity(hintCount<2 ? 1 : 0)
+                Text(artistName).font(.system(size: 14, weight: .semibold)).opacity(hintCount<2 ? 1 : 0)
             }
             
             if let releaseDate = musicManager.song?.releaseDate {
                 let dateString = dateToString(releaseDate)
-                Text(dateString).padding(.top, 30).opacity(hintCount < 1 ? 1 : 0)
+                Text(dateString).font(.system(size: 10, weight: .semibold)).opacity(hintCount < 1 ? 1 : 0)
             }
            
             Button(action:{
@@ -80,6 +84,7 @@ struct QuizView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 50, height: 50)
                     .foregroundColor(Color(hex: "#000000"))
+                    .shadow(color: .gray, radius: 1, x: 0, y: 3)
                 
             }
             .padding(.top,20)
@@ -88,20 +93,19 @@ struct QuizView: View {
                 hintCount-=1
                 print(hintCount)
                 
-                if(currentQuestionCount == questionCount){
-                    NavigationManager.shared.push(to: .quizFinish)
+                if (hintCount < 0){
+                    isCorrect = true
                 }
                 
-                else{
-                    if (hintCount < 0){
-                        isCorrect = true
-                    }
+                if(hintCount < -1){
+                    print("다음문제로 넘어갑니다.")
+                    hintCount = 3
+                    isCorrect = false
                     
-                    if(hintCount < -1){
-                        print("다음문제로 넘어갑니다.")
-                        hintCount = 3
-                        isCorrect = false
-                        
+                    if(currentQuestionCount == questionCount){
+                        NavigationManager.shared.push(to: .quizFinish)
+                    }
+                    else{
                         musicManager.getRandomTrack(time: Double(playTime))
                         currentQuestionCount+=1
                         print(currentQuestionCount)
@@ -109,10 +113,10 @@ struct QuizView: View {
                 }
             }){
                 ZStack{
-                    RoundedRectangle(cornerRadius: 40).frame(width: 92,height: 38).foregroundColor(Color(hex: "#000000"))
+                    RoundedRectangle(cornerRadius: 40).frame(width: 135,height: 50).foregroundColor(Color(hex: "#000000")).shadow(color: .gray, radius: 1, x: 0, y: 3)
                     Text(hintCount < 1 ?
                          hintCount < 0 ?
-                         "다음문제":"정답보기" : "힌트 \(hintCount) / 3").font(.system(size: 18, weight: .bold)).foregroundColor(.white)
+                         "다음문제":"정답보기" : "힌트 \(hintCount)/3").font(.system(size: 18, weight: .bold)).foregroundColor(.white)
                 }
             }.padding(.top,10)
             
@@ -120,10 +124,11 @@ struct QuizView: View {
                 hintCount = -1
                 isCorrect = true
             }){
-                Text("정답 바로보기").font(.system(size: 18, weight: .regular)).foregroundColor(.black)
-            }
-            .padding(.top, 20)
+                Text("정답 바로보기").font(.system(size: 12, weight: .regular)).foregroundColor(.black).opacity(hintCount > 0 ? 1 : 0)
+            }.disabled(hintCount <= 0)
+            .padding(.top, 40)
         }
+        .padding(.horizontal,16)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button {
